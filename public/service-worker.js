0b7b78e6a0fdc3ce6ce54e6c2ba79769f4245307
Allen DeHoff <allen.dehoff21@gmail.com>
2020-11-12 09:42:26 -0700

    committing everything again, seems nothing is on github

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,8 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/index.js",
+    "/db.js",
+    "/style.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
     "/manifest.webmanifest",
